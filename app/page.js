f9add9a07487f32361fdb5cd711f1c537218eed7
Sajeer <87226062+SajeerFullstackDev@users.noreply.github.com>
2025-04-04 13:05:32 +0530

    fix

--- app/page.js
+++ app/page.js
@@ -11,10 +11,10 @@ export default function Home() {
     <>
       <Header />
 
-      <div className="bg-black text-white">
+      <div className="bg-[#0B0E14] text-white">
         <div className="container mx-auto px-6 lg:px-20 py-16 flex flex-col lg:flex-row items-center">
           <div className="lg:w-1/2 space-y-6">
-            <h2 className="text-4xl lg:text-5xl font-bold">
+            <h2 className="text-5xl lg:text-5xl font-bold">
               Turn your devices into kiosks in a few minutes with Hexnode UEM
             </h2>
             <Form />
@@ -24,11 +24,57 @@ export default function Home() {
             <img
               src="/hexnode-kiosk.webp"
               alt="Devices showcasing Hexnode"
-              className="w-full max-w-lg"
+              className="w-full max-w-xl"
             />
           </div>
         </div>
       </div>
+
+
+
+
+
+      <div
+        className="text-white py-8"
+        style={{ backgroundColor: "oklch(0.22 0.03 270.85)" }}
+      >
+        <div className="max-w-6xl mx-auto px-6 grid grid-cols-1 md:grid-cols-3 gap-8 text-center md:text-left  border-gray-700 pt-6">
+          <div className="flex flex-col items-center md:items-start">
+            <img src="/logos/idc.webp" alt="IDC" className="h-10 mb-2" />
+            <p className="text-gray-300 text-sm">
+              Hexnode is listed as a leader and a major player in IDC
+              MarketScape UEM Vendors Assessment Reports 2024.
+            </p>
+          </div>
+
+          <div className="flex flex-col items-center md:items-start sm:border-none md:border-l border-gray-700 pl-6">
+
+            <img
+              src="/logos/gartner.webp"
+              alt="Gartner"
+              className="h-10 mb-2"
+            />
+            <p className="text-gray-300 text-sm">
+              Hexnode was recognized in the 2025 Gartner® Market Guide for
+              Unified Endpoint Management Tools.
+            </p>
+          </div>
+
+          <div className="flex flex-col items-center md:items-start sm:border-none md:border-l border-gray-700 pl-6">
+
+            <img
+              src="/logos/forrester.webp"
+              alt="Forrester"
+              className="h-10 mb-2"
+            />
+            <p className="text-gray-300 text-sm">
+              Forrester includes Hexnode as a Notable vendor in The Unified
+              Endpoint Management Landscape, Q3 2023.
+            </p>
+          </div>
+        </div>
+      </div>
+
 
       <Tabs />
 
@@ -41,40 +87,35 @@ export default function Home() {
         <SwiperSlider />
       </div>
 
-
-
-
-      
       <section className="text-center py-16 bg-white">
-  <div className="container mx-auto px-4">
-    <h2 className="text-3xl font-semibold text-gray-900 mb-10">
-      Platforms supported
-    </h2>
-    <div className="flex justify-center items-center gap-8 mt-8 flex-wrap">
-      {[
-        { name: "Android", img: "/logos/android.svg" },
-        { name: "Windows", img: "/logos/windows.svg" },
-        { name: "iOS", img: "/logos/ios.svg" },
-        { name: "Android TV", img: "/logos/android-tv.svg" },
-        { name: "Apple TV", img: "/logos/apple-tv.svg" },
-        { name: "Fire", img: "/logos/fire.webp" },
-      ].map((platform, index) => (
-        <div 
-          key={index} 
-          className="border p-4 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300"
-        >
-          <img 
-            src={platform.img} 
-            alt={platform.name} 
-            className="h-12 w-auto" 
-            loading="lazy"
-          />
+        <div className="container mx-auto px-4">
+          <h2 className="text-3xl font-semibold text-gray-900 mb-10">
+            Platforms supported
+          </h2>
+          <div className="flex justify-center items-center gap-8 mt-8 flex-wrap">
+            {[
+              { name: "Android", img: "/logos/android.svg" },
+              { name: "Windows", img: "/logos/windows.svg" },
+              { name: "iOS", img: "/logos/ios.svg" },
+              { name: "Android TV", img: "/logos/android-tv.svg" },
+              { name: "Apple TV", img: "/logos/apple-tv.svg" },
+              { name: "Fire", img: "/logos/fire.webp" },
+            ].map((platform, index) => (
+              <div
+                key={index}
+                className="border p-4 rounded-lg shadow-md hover:shadow-lg transition-shadow duration-300"
+              >
+                <img
+                  src={platform.img}
+                  alt={platform.name}
+                  className="h-12 w-auto"
+                  loading="lazy"
+                />
+              </div>
+            ))}
+          </div>
         </div>
-      ))}
-    </div>
-  </div>
-</section>
-
+      </section>
 
       <section className="bg-gray-900 text-white text-center py-10">
         <h2 className="text-2xl font-semibold">
